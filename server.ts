7b00066252d6132a38ffae94f23071e3b3f9249c
Aaron Miller <aaronmiller@gmail.com>
2024-09-04 09:59:33 -0700

    home grown zip

--- server.ts
+++ server.ts
@@ -1,9 +1,38 @@
 import { Hono } from "https://deno.land/x/hono/mod.ts";
 import { serve } from "https://deno.land/std/http/mod.ts";
 import { processJsonInMemory } from "./split.ts";
-import { zip } from "https://deno.land/x/zipjs/mod.ts";
 
 const app = new Hono();
+
+// Simple ZIP creation function
+function createZip(files: Record<string, string>): Uint8Array {
+  const encoder = new TextEncoder();
+  const zipParts: Uint8Array[] = [];
+
+  for (const [filename, content] of Object.entries(files)) {
+    const fileData = encoder.encode(content);
+    const header = new Uint8Array([
+      0x50, 0x4B, 0x03, 0x04, 0x0A, 0x00, 0x00, 0x00, 0x00, 0x00,
+      ...new Uint8Array(new Uint32Array([Date.now()]).buffer),
+      ...new Uint8Array(new Uint32Array([fileData.length]).buffer),
+      ...new Uint8Array(new Uint32Array([fileData.length]).buffer),
+      ...new Uint16Array([filename.length]).buffer,
+      0x00, 0x00,
+    ]);
+
+    zipParts.push(header, encoder.encode(filename), fileData);
+  }
+
+  const centralDirectory = new Uint8Array([
+    0x50, 0x4B, 0x05, 0x06, 0x00, 0x00, 0x00, 0x00,
+    ...new Uint16Array([Object.keys(files).length]).buffer,
+    ...new Uint16Array([Object.keys(files).length]).buffer,
+    ...new Uint32Array([zipParts.reduce((acc, part) => acc + part.length, 0)]).buffer,
+    0x00, 0x00, 0x00, 0x00,
+  ]);
+
+  return new Uint8Array([...zipParts.flat(), ...centralDirectory]);
+}
 
 // Serve the upload form
 app.get("/", async (c) => {
@@ -28,14 +57,7 @@ app.post("/upload", async (c) => {
   const splitFiles = await processJsonInMemory(fileContent);
 
   // Create a zip file in memory
-  const zipWriter = new zip.ZipWriter(new zip.Uint8ArrayWriter());
-
-  for (const [filename, content] of Object.entries(splitFiles)) {
-    await zipWriter.add(filename, new zip.TextReader(content));
-  }
-
-  // Generate the zip content
-  const zipContent = await zipWriter.close();
+  const zipContent = createZip(splitFiles);
 
   console.log(`Zip file created in memory, size: ${zipContent.length} bytes`);
 
